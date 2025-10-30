5a89e271dbe1c866a2b8105eee0b35d2c3d47351
nikhilpatankar1998 <nikhilgithub@321>
2025-10-30 12:09:43 +0530

    parse browser, plateform

--- backend/utils/logUserActivity.js
+++ backend/utils/logUserActivity.js
@@ -1,16 +1,24 @@
+const UAParser = require('ua-parser-js');
 const UserActivityLog = require('../models/UserActivityLog');
+
 
 const logUserActivity = async (req, user, activity) => {
   try {
     const userAgent = req.headers['user-agent'] || 'Unknown';
+    const parser = new UAParser(userAgent);
+    const uaResult = parser.getResult();
     const {timezone,resolution}= req.body;
+
+    // Extract clean info
+    const browser = uaResult.browser.name || 'Unknown';
+    const platform = uaResult.os.name || 'Unknown';
 
     await UserActivityLog.create({
       user_id: user.id,
       email: user.email,
       activity,
-      browser: userAgent,
-      platform: userAgent, // you can later parse this if needed
+      browser,
+      platform, // you can later parse this if needed
       timezone,
       resolution,
     });
